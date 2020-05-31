b4df6597f5c52dec6d29e65e52104314ea1e60f1
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-05-31 15:14:39 -0700

    Assignment 10 Doubles

--- spec/assignments_spec.rb
+++ spec/assignments_spec.rb
@@ -67,6 +67,7 @@ Assignment 9: Compound Expectations
 3. Write a compound expectation that asserts that the array [4, 8, 15, 16, 23, 42] includes the value 42 and starts with the values 4, 8, and 15.
 =end
 
+=begin
 #1.
 
 RSpec.describe 'firetruck tester' do 
@@ -76,16 +77,64 @@ RSpec.describe 'firetruck tester' do
     end
 end
 
-
+#2
 RSpec.describe 20 do
     it 'is even and responds to the times method' do
         expect(subject).to be_even.and respond_to(:times)
     end
 end
 
+#3
 RSpec.describe 'assignment 3' do
     let(:arr) { return [4, 8, 15, 16, 23, 42]}
     it 'does assignment 3' do
         expect(arr).to include(42).and start_with(4, 8, 15)
     end
+end
+=end
+
+=begin 
+Assignment 10: Doubles
+
+Paste your code solutions to each question below. 
+You can assume the basic boilerplate code for a test suite has already been written (see below).
+
+RSpec.describe 'doubles' do
+  it "tests the student's knowledge of the course's content" do
+    # For each question, write the code that should go here
+  end
+end
+
+Questions for this assignment
+Create a double with the name "Database Connection". 
+The double should have a method called connect that returns the value true.
+The double also have a method called disconnect that returns the value "Goodbye".
+The double's methods should be assigned in the initial invocation of the double method.
+Write two expectations, one for connect and one for disconnect, that confirms the return value of each.
+
+Assign the double to the variable db.
+Create a double with the name "File System". Assign the double to the variable fs.
+Using the allow method, give the double a read method that returns the value "Romeo and Juliet".
+Using the allow method, give the double a write method that returns the value false.
+=end
+
+RSpec.describe 'doubles' do
+  it "tests the student's knowledge of the course's content" do
+    db_connection = double('Database Connection')    
+    allow(db_connection).to receive(:connect).and_return(true)
+    allow(db_connection).to receive(:disconnect).and_return('Goodbye')
+    
+    expect(db_connection.connect).to eq(true)
+    expect(db_connection.disconnect).to eq('Goodbye')
+  end
+
+    it 'does test 2' do
+    fs = double('File System')
+    allow(fs).to receive(:read).and_return('R and J')
+    allow(fs).to receive(:write).and_return(false)
+
+    expect(fs.read).to eq('R and J')
+    expect(fs.write).to eq(false)
+
+  end
 end
