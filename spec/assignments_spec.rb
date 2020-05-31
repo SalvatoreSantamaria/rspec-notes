a15bb71785abc11a508e1e3f23220f97c5600677
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-05-31 14:14:42 -0700

    Assignment 9: Compound Expectations

--- spec/assignments_spec.rb
+++ spec/assignments_spec.rb
@@ -7,13 +7,16 @@ The eq method should compare the result fo the evaluation with the right answer.
 =end
 
 RSpec.describe 'math calculations' do
-    it('does bastic math') do
+    it('does basic math') do
         expect(0 + 1).to eq(1)
         expect(1 + 1).to eq(2)
     end
 end
 
 =begin
+
+#Assignment 8: Predicate, all and be Matchers
+
 Questions for this assignment
 1. What is an alternate way to write the assertion below?
 expect({}.empty?).to be(true)
@@ -39,6 +42,7 @@ nil
 [1, 2, 3]
 =end
 
+=begin
 #2
 RSpec.describe 'array values even tester' do
     let(:arr) { return [10, 20, 30]}
@@ -54,3 +58,34 @@ RSpec.describe 'array values positive tester' do
     end
 end
 
+=end
+
+=begin
+Assignment 9: Compound Expectations
+1. Write a compound expectation that asserts the string firetruck starts with the substring "fire" and ends with the substring "truck".
+2. Write a compound expectation that asserts that the number 20 is even and that it responds to the times mthod.
+3. Write a compound expectation that asserts that the array [4, 8, 15, 16, 23, 42] includes the value 42 and starts with the values 4, 8, and 15.
+=end
+
+#1.
+
+RSpec.describe 'firetruck tester' do 
+    let(:firetruck) { return 'firetruck' }
+    it 'asserts string firetruck starts with the substring "fire" and ends with the substring "truck"' do
+        expect(firetruck).to start_with('fire').and end_with('truck')
+    end
+end
+
+
+RSpec.describe 20 do
+    it 'is even and responds to the times method' do
+        expect(subject).to be_even.and respond_to(:times)
+    end
+end
+
+RSpec.describe 'assignment 3' do
+    let(:arr) { return [4, 8, 15, 16, 23, 42]}
+    it 'does assignment 3' do
+        expect(arr).to include(42).and start_with(4, 8, 15)
+    end
+end
