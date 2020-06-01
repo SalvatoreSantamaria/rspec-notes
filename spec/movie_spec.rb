ab5da8e5df37e3b2a21a3b80fe838af73367da7f
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-06-01 08:25:27 -0700

    set up our test movie with my example

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -43,20 +43,85 @@ end
 # movie = Movie.new(actor)
 # movie.start_shooting
 
+
+#Replacing and object with a double.
+#Goal here is to seperate the movie class from the actor class
 RSpec.describe Movie do
-  let(:stuntman) { double("Mr. Danger", ready?: true, act: 'Any string at all', fall_off_ladder: 'Sure! Lets do it', light_on_fire: true)}
+  let(:stuntman) { 
+    return double("Mr. Danger", 
+      #mimic-ing the real methods here. there are all 'fake' or mock methods
+      ready?: true, 
+      act: 'Any string at all', 
+      fall_off_ladder: 'Sure! Lets do it', 
+      light_on_fire: true
+      )}
   subject { described_class.new(stuntman) }
 
   describe '#start_shooting method' do
     it 'expects an actor to do 3 actions' do
+
+      #if I want to test the actions of the methods below, 
+      #those would get tested in the ACTOR class (actor.ready?, actor.act, etc...)
+      expect(stuntman).to receive(:ready?)
+      expect(stuntman).to receive(:act)
+      expect(stuntman).to receive(:fall_off_ladder)
+      expect(stuntman).to receive(:light_on_fire)
+
+
       # expect(stuntman).to receive(:light_on_fire).once
       # expect(stuntman).to receive(:light_on_fire).exactly(1).times
-      expect(stuntman).to receive(:light_on_fire).at_most(1).times
+      #expect(stuntman).to receive(:light_on_fire).at_most(1).times
 
       # expect(stuntman).to receive(:act).twice
       # expect(stuntman).to receive(:act).exactly(2).times
-      expect(stuntman).to receive(:act).at_least(2).times
+      #expect(stuntman).to receive(:act).at_least(2).times
       subject.start_shooting
+    end
+  end
+end
+
+
+#own example
+
+
+class Actor2
+  def initialize(name)
+    @name = name
+  end
+
+  def a2_method
+    puts "Actor2.a2_method"
+  end
+end
+
+class Movie2
+  attr_reader :actor
+
+  def initialize(actor)
+    @actor = actor
+  end
+
+  def m2_method
+      actor.a2_method
+  end
+end
+
+
+#Replacing and object with a double.
+#Goal here is to seperate the movie class from the actor class
+RSpec.describe Movie2 do
+  let(:stuntman) { 
+    return double("Mr. Danger", 
+      #mimic-ing the real methods here. there are all 'fake' or mock methods
+      a2_method: 'Any string at all', 
+      )}
+  subject { described_class.new(stuntman) }
+
+  describe '#start_shooting method' do
+    it 'expects an actor to do 1 actions' do
+
+      expect(stuntman).to receive(:a2_method)
+      subject.m2_method
     end
   end
 end
