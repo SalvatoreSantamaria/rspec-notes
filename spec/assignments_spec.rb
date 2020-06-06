3c5e0dc31d01643e1a6fadacaeb6dc96e02e6e01
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-06-06 07:05:32 -0700

    Assignment 7: Subjects, Shared Examples, and Shared Context

--- spec/assignments_spec.rb
+++ spec/assignments_spec.rb
@@ -1,4 +1,48 @@
 =begin
+
+1.By default, what will subject equal? 
+    #the instance of the class under test
+2. How do you define a custom subject? Provide a code example.
+    #invoke the subject method and proide a Ruby block. The final evaluation of the block with be the value of subject
+    #whenever its used in an example.
+    subject {'hello'}
+3. How does the described_class helper method work? What is the advantage of using it over writing the class name out?
+    #descibed_class dynamically references the class being testing (the one passed as an argument to the top-leve
+    describe method). The advantage is that if the class name cahnges the rest of the spec does not have to change. 
+
+4. Write an RSpec test for an array with 2 elements.
+    Declare the subject to be [1, 2].
+    Write an example that uses expect(subject) to test whether the array is equal to [1, 2].
+    Write an example that uses the one-liner is_expected syntax.
+Which do you prefer?
+5. What's the difference between include_examples and include_context?
+    include_examples method injects predefined examples into an example group. 
+    include_context method injects context (like before blocks, instance variables, helper methods, let variables)
+        into an example group.
+    both remove ruplication across tests
+=end
+
+#4. 
+
+Rspec.describe Array do
+    subject(:somearray) do 
+        [1, 2]
+    end
+  it 'should be an array with 2 elements' do
+    expect(subject).to eq([1, 2])
+  end
+
+    it {is_expected.to eq([1, 2])} #one liner
+
+end
+
+
+
+
+
+
+
+=begin
 Create an example group with a string argument of "math calculations".
 Inside the group, create an example with a string argument of "does basic math".
 Inside the example, write 4 mathematical assertions of your choice.
