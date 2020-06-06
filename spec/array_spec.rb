43ebbf9b665ad838afff7c7404c5f5a85608fa45
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-06-06 06:30:02 -0700

    Added notes to assignment 5

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe Array do 
     it 'can get pushed onto' do
         expect(subject.length).to eq(0)
-
+        #Subject helper returns an instance of the class under test
 
         subject.push(1)
         expect(subject.length).to eq(1)
+        #With a class argument, RSpec is able to instantiate an instance of the class 
+        #and make it lazily available via the subject method. 
     end
 end
